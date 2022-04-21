25d17a1d5f404ba31da3c7702990c053173a80a9
Macbook air <engineerhoshang33@gmail.com>
2022-04-21 10:16:49 +0300

    adding photos to Art

--- src/pages/Art.tsx
+++ src/pages/Art.tsx
@@ -1,10 +1,33 @@
 import React from "react";
+import Image from "next/image";
+import Van1 from "/Users/macbookair/Documents/Project/art-site/public/image/Van1.jpeg";
+import Van2 from "/Users/macbookair/Documents/Project/art-site/public/image/Van2.jpeg";
+import Van3 from "/Users/macbookair/Documents/Project/art-site/public/image/Van3.jpeg";
+
 
 const Art = ()=> {
     return (
-        <div>
-            <h1>Art</h1>
-        </div>
+        <div className="grid grid-cols-3 gap-4 justify-items-center p-4">
+  <div className="hover:scale-110 shadow-lg shadow-cyan-500/50">
+<Image src={Van1} width={400} height={400}/>
+  </div>
+  <div className="hover:scale-110 shadow-lg shadow-cyan-500/50">
+  <Image src={Van2} width={400} height={400}/>
+  </div>
+  <div className="hover:scale-110 shadow-lg shadow-cyan-500/50">
+  <Image src={Van3} width={400} height={400}/>
+  </div>
+  <div className="hover:scale-110 shadow-lg shadow-cyan-500/50">
+<Image src={Van1} width={400} height={400}/>
+  </div>
+  <div className="hover:scale-110 shadow-lg shadow-cyan-500/50">
+  <Image src={Van2} width={400} height={400}/>
+  </div>
+  <div className="hover:scale-110 shadow-lg shadow-cyan-500/50">
+  <Image src={Van3} width={400} height={400}/>
+  </div>
+  
+</div>
     );
 }
 
